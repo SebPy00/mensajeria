726f45a49f477de0023968abcef143fcb12ffb38
aibarrola <anibarrola@sistemasygestiones.com.py>
2022-09-26 11:47:23 -0400

    cambios finales

---
+++ app/Models/EnvioMensajesDetalle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EnvioMensajesDetalle extends Model
+{
+    use HasFactory;
+
+    protected $table = 'envio_mensaje_detalle';
+
+}
