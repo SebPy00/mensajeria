936d2a4fc8be1b488b26b17fc764f3afa90d7209
esperalta <esperalta@sistemasygestiones.com.py>
2024-01-04 09:27:14 -0300

    ajustes

--- app/Exports/GenerarBaseCobrosRuralCobranzas.php
+++ app/Exports/GenerarBaseCobrosRuralCobranzas.php
@@ -29,7 +29,7 @@ class GenerarBaseCobrosRuralCobranzas implements FromCollection, WithHeadings
             foreach ($cobros as $cob){
                 $carbonFecha = Carbon::createFromFormat('Y-m-d', trim($cob->fec_pago));
                 $fechaPago = $carbonFecha->format('j/n/Y');
-                $saldoDet3 = trim($cob->saldo);
+                $saldoDet3 = (string) $cob->saldo;
                 $saldoFormateado = str_replace(',', '.', $saldoDet3);
                 $fila = [
                     'Numero Documento'=> $cob->nrodocumento,
@@ -37,7 +37,7 @@ class GenerarBaseCobrosRuralCobranzas implements FromCollection, WithHeadings
                     'Cliente DET3'=> '',
                     'Operacion DET3'=> $cob->operacion,
                     'Cartera DET3'=> $cob->cartera,
-                    'Saldo DET3'=> trim($cob->saldo),
+                    'Saldo DET3'=> $saldoFormateado,
                     'Fecha Pago DET3'=> $fechaPago,
                     'Monto Pagado DET3'=> $cob->mon_pagado,
                     'Numero Cuota DET3'=> $cob->nro_cuota,
