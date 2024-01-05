44b7ceaca48aee0b685d574702e3088a2dcb61f1
esperalta <esperalta@sistemasygestiones.com.py>
2024-01-05 12:43:22 -0300

    ajuste

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -149,8 +149,8 @@ class Kernel extends ConsoleKernel
         //CONSUMO DE API DE CLIENTE E INSERCION DE ASIGNACION EN BASE SIGESA
 	    $schedule->command('clientes:alarmas')->daily()->at('7:00');
 	    $schedule->command('cobros:alarmas')->daily()->at('7:15'); //ALARMASPY
-        $schedule->command('clientes:cph')->daily()->at('7:20');
-        $schedule->command('cobros:cph')->daily()->at('7:30');
+        $schedule->command('clientes:cph')->daily()->at('7:20');//CPH
+        $schedule->command('cobros:cph')->daily()->at('7:30');//CPH
         //GENERACION DE ARCHIVOS DE BASE PARA SERVICIOS
   	    $schedule->command('generar:alarmaspy')->daily()->at('7:40'); //ALARMASPY
         $schedule->command('generar:ruralcobranzas')->daily()->at('7:50'); //RuralCobranzas
