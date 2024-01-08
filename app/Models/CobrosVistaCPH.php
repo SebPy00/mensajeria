7ab748f0bf5d018f719af1a26a9c8a1a6eb43f5b
esperalta <esperalta@sistemasygestiones.com.py>
2024-01-08 15:15:47 -0300

    ajustes CPH

--- app/Models/CobrosVistaCPH.php
+++ app/Models/CobrosVistaCPH.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CobrosCPH extends Model
+class CobrosVistaCPH extends Model
 {
     use HasFactory;
     protected $connection = 'servicios';
