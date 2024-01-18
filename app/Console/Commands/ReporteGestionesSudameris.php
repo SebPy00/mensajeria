15786ba3a420e238681c877423e539432796bc6a
esperalta <esperalta@sistemasygestiones.com.py>
2024-01-18 18:05:34 -0300

    sudameris

---
+++ app/Console/Commands/ReporteGestionesSudameris.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Exports\ReporteGestionesSudamerisExport;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Maatwebsite\Excel\Facades\Excel;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
+use App\Models\GestionesSudameris;
+
+class ReporteGestionesSudameris extends Command
+{
+    protected $signature = 'gestiones:sudameris {inicio?} {fin?}';
+
+    protected $description = 'Comando para la generación del reporte de gestiones diarias de Sudameris';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        log::info('INICIA REPORTE DE GESTION DIARIA SUDAMERIS');
+
+        // Obtén las fechas de inicio y fin desde los argumentos o pide la entrada del usuario si no se proporcionan
+        $inicio = $this->argument('inicio') ?: $this->ask('Ingrese la fecha de inicio (formato: YYYY-MM-DD):');
+        $fin = $this->argument('fin') ?: $this->ask('Ingrese la fecha de fin (formato: YYYY-MM-DD):');
+
+        // Valida que las fechas sean válidas
+        try {
+            $fechaInicio = Carbon::parse($inicio);
+            $fechaFin = Carbon::parse($fin);
+        } catch (\Exception $e) {
+            $this->error('Error al analizar las fechas. Asegúrese de ingresar fechas válidas en formato YYYY-MM-DD.');
+            return;
+        }
+
+        $registros = GestionesSudameris::whereBetween('fecha_hora', [$fechaInicio, $fechaFin])->get();
+
+        // Verifica si hay registros antes de proceder con la exportación
+        if ($registros->count() > 0) {
+            // Genera y almacena el archivo TXT de forma asíncrona
+            $nombreArchivo = 'GESTIONES_' . $fechaInicio->format('Ymd') . '_' . $fechaFin->format('Ymd') . '.txt';
+            Excel::store(new ReporteGestionesSudamerisExport($registros), $nombreArchivo, 's7');
+
+            // Mueve el archivo después de almacenarlo
+            //$this->moverArchivo($nombreArchivo);
+        } else {
+            $this->info('No hay registros para el rango de fechas especificado.');
+        }
+    }
+
+    private function moverArchivo($nombreArchivo)
+    {
+        log::info('Mueve el archivo');
+        $origen = 's7'; // Asegúrate de que este sea el disco correcto según tu configuración
+        $destino = 's10'; // Asegúrate de que este sea el disco correcto según tu configuración
+
+        Storage::disk($destino)->put($nombreArchivo, Storage::disk($origen)->get($nombreArchivo));
+    }
+}
---
+++ app/Exports/ReporteGestionesSudamerisExport.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\GestionesSudameris;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Log;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithCustomCsvSettings;
+
+class ReporteGestionesSudamerisExport implements FromCollection, WithHeadings, WithMapping, WithCustomCsvSettings
+{
+    protected $registros;
+
+    public function __construct($registros)
+    {
+        $this->registros = $registros;
+    }
+
+    public function collection()
+    {
+        log::info('INICIA REPORTE DE GESTION DIARIA SUDAMERIS');
+
+        $gestiones = $this->registros;
+
+        $lista = [];
+
+        if (isset($gestiones->Table) && is_array($gestiones->Table)) {
+            foreach ($gestiones->Table as $elemento) {
+                $fgestion = new Carbon($elemento->FECHA_GESTION);
+                $freagenda = new Carbon($elemento->FECHA_REAGENDADA);
+
+                $fila = [
+                    'base' => $elemento->BASE,
+                    'id_contacto' => $elemento->ID_CONTACTO,
+                    'doc' => $elemento->DOC,
+                    'cod_cliente' => $elemento->COD_CLIENTE,
+                    'nombre_cliente' => $elemento->NOMBRE_CLIENTE,
+                    'categoria' => $elemento->CATEGORIA,
+                    'sub_categoria' => $elemento->SUB_CATEGORIA,
+                    'id_comentario' => $elemento->ID_COMENTARIO,
+                    'comentario' => $elemento->COMENTARIO,
+                    'fecha_hora' => $fgestion->toDateTimeString(),
+                    'fecha_agenda' => $freagenda->toDateTimeString(),
+                    'telefono' => $elemento->TELEFONO,
+                    'usuario' => $elemento->USUARIO,
+                    'operacion' => $elemento->OPERACION,
+                ];
+
+                if ($elemento->RESP_CORTA != 'Cerrado por Proceso') {
+                    $lista[] = $fila;
+                }
+            }
+        }
+
+        return collect($lista);
+    }
+
+    public function headings(): array
+    {
+        // Define los encabezados de las columnas en el archivo
+        return [
+            'base',
+            'id_contacto',
+            'doc',
+            'cod_cliente',
+            'nombre_cliente',
+            'categoria',
+            'sub_categoria',
+            'id_comentario',
+            'comentario',
+            'fecha_hora',
+            'fecha_agenda',
+            'telefono',
+            'usuario',
+            'operacion',
+        ];
+    }
+
+    public function map($fila): array
+    {
+        // Mapea cada valor de la fila para personalizar la salida
+        return $fila;
+    }
+
+    public function getCsvSettings(): array
+    {
+        // Configuración personalizada del CSV
+        return [
+            'delimiter' => ';',
+            'enclosure' => '',
+            'line_ending' => "\n",
+        ];
+    }
+}
