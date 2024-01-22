1fc0376e6a155fa5894420aa4d8bd7463980cc01
esperalta <esperalta@sistemasygestiones.com.py>
2024-01-22 12:23:36 -0300

    AJUSTES EN EL LOG

--- app/Console/Commands/ClearLaravelLog.php
+++ app/Console/Commands/ClearLaravelLog.php
@@ -22,10 +22,8 @@ class ClearLaravelLog extends Command
 
             // Mantener solo las entradas de los últimos 15 días
             $cutOffDate = Carbon::now()->subDays(15)->format('Y-m-d H:i:s');
-            $filteredContent = preg_replace("/^\[.*?\] local\.[^\n]*\n?/m", "", $content);
-            $filteredContent = preg_replace("/^\[.*?\] production\.[^\n]*\n?/m", "", $filteredContent);
 
-            $lines = explode("\n", $filteredContent);
+            $lines = explode("\n", $content);
             $filteredLines = array_filter($lines, function ($line) use ($cutOffDate) {
                 return empty($line) || Carbon::parse(substr($line, 1, 19))->gt($cutOffDate);
             });
@@ -33,7 +31,7 @@ class ClearLaravelLog extends Command
             $finalContent = implode("\n", $filteredLines);
 
             File::put($logFilePath, $finalContent);
-            $this->info("Se limpiaron los logs, se mantuvieron los ultimos 15 dias en {$logFile}.");
+            $this->info("Kept entries from the last 15 days in {$logFile}.");
         } else {
             $this->info("{$logFile} not found.");
         }
