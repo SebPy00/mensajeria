86cb9ea5babe6cd54e0bf5984b27144b6513ab3b
esperalta <esperalta@sistemasygestiones.com.py>
2024-01-22 10:36:09 -0300

    CPH gestiones

--- app/Console/Commands/InsertGestionesCPH.php
+++ app/Console/Commands/InsertGestionesCPH.php
@@ -86,8 +86,8 @@ class InsertGestionesCPH extends Command
         //$fechaDesde = $fecha .' 00:00:00';
         //$fechaHasta = $fecha .' 23:59:59';
 
-        $fechaDesde = '2024-01-12 00:00:00'; //Usar para tener gestiones de dias anteriores
-        $fechaHasta = '2024-01-12 23:59:59'; //Usar para tener gestiones de dias anteriores
+        $fechaDesde = '2024-01-13 00:00:00'; //Usar para tener gestiones de dias anteriores
+        $fechaHasta = '2024-01-21 23:59:59'; //Usar para tener gestiones de dias anteriores
 
         $client = new Client();
         try {
