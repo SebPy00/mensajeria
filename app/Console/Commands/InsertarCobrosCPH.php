7c3eeb20a14fdd30e9d259317ddb14fb766342b5
esperalta <esperalta@sistemasygestiones.com.py>
2024-01-24 11:40:27 -0300

    cph

--- app/Console/Commands/InsertarCobrosCPH.php
+++ app/Console/Commands/InsertarCobrosCPH.php
@@ -67,8 +67,8 @@ class InsertarCobrosCPH extends Command
             }
             $fechaDesde = '2024-'.$mes.'-01';*/
 
-            $fechaDesde = '2023-11-01';
-            $fechaHasta = '2023-12-31';
+            $fechaDesde = '2024-01-01';
+            $fechaHasta = '2024-01-23';
 
 
             //OBS:  <inf:Doc>CI</inf:Doc> PARA TRAER UN SOLO CLIENTE
