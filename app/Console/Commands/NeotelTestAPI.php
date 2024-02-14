4b177840eb692b708d8583f3aa49d049d5f37911
esperalta <esperalta@sistemasygestiones.com.py>
2024-02-14 14:22:57 -0300

    ajustes de test

--- app/Console/Commands/NeotelTestAPI.php
+++ app/Console/Commands/NeotelTestAPI.php
@@ -2,131 +2,107 @@
 
 namespace App\Exports;
 
-use Maatwebsite\Excel\Concerns\FromCollection;
-use Maatwebsite\Excel\Concerns\Exportable;
-use Maatwebsite\Excel\Concerns\WithHeadings;
 use Illuminate\Support\Facades\Log;
 use App\Models\DatosEjemplo;
 use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
-class GenerarBaseClientesNeotelExport implements FromCollection, WithHeadings
+class GenerarBaseClientesNeotelExport
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    use Exportable;
-    public function collection()
+    public function export()
     {
-        log::info('INICIA GENERACION DE BASE CLIENTES NEOTEL');
+        Log::info('INICIA GENERACION DE BASE CLIENTES NEOTEL');
 
         $fecha = Carbon::now()->toDateString();
         $clientes = $this->getClientes($fecha);
         $lista = [];
+
         if (isset($clientes)) {
-            foreach ($clientes as $cli){
+            foreach ($clientes as $cli) {
                 $vto = new Carbon($cli->fecha_vto_cuota);
-                $apertura =new Carbon($cli->fec_apertura);
+                $apertura = new Carbon($cli->fec_apertura);
 
                 $fila = [
-                    'cod_cliente'=>trim($cli->cod_cliente),
-                    'nro_documento'=>trim($cli->nro_documento),
-                    'nom_cliente'=>trim($cli->nom_cliente),
-                    'tellab'=>trim($cli->tellab),
-                    'telefono'=>trim($cli->telefono),
-                    'cel_alternativo'=>trim($cli->cel_alternativo),
-                    'cel_lab'=>trim($cli->cel_lab),
-                    'cel_part'=>trim($cli->cel_part),
-                    'operacion'=>trim($cli->operacion),
-                    'segmento'=>'',
-                    'producto'=>trim($cli->producto),
-                    'tipo_cred_tarj'=>'',
-                    'tipo_ope'=>'',
-                    'tasa'=>'',
-                    'saldo_capital'=>trim($cli->saldo_capital),
-                    'saldo_interes'=>trim($cli->saldo_interes),
-                    'tipo_cambio'=>'',
-                    'dias_mora'=>trim($cli->dias_mora),
-                    //'nro_cuota'=>trim($cli->nro_cuota),
-                    'nro_cuota'=>'1',
-                    //'total_cuotas'=>trim($cli->total_cuotas),
-                    'total_cuotas'=>'1',
-                    //'cuotas_pag'=>trim($cli->cuotas_pag),
-                    'cuotas_pag'=>'0',
-                    //'cuotas_pend'=>trim($cli->cuotas_pend),
-                    'cuotas_pend'=>'1',
-                    'monto_mora'=>'',
-                    'monto_cuota'=>trim($cli->monto_cuota),
-                    'fecha_vto_cuota'=>$vto->format('d/m/Y'),
-                    'ult_fech_pago'=>'',
-                    'total_deuda'=>'',
-                    'estado'=>'',
-                    'fec_apertura'=> $apertura->format('d/m/Y'),
-                    'cat_riesgo'=>'',
-                    'dato_extra1'=>trim($cli->dato_extra1),
-                    'dato_extra2'=>trim($cli->dato_extra2),
-                    'dato_extra3'=>trim($cli->dato_extra3),
-                    'dato_extra4'=>trim($cli->dato_extra4),
-                    //'dato_extra5'=>trim($cli->dato_extra5),
-                    'dato_extra5'=>'',
-                    'dato_extra6'=>'',
-                    'dato_extra7'=>'',
-                    'COD_GESTOR'=>''
+                    'cod_cliente' => trim($cli->cod_cliente),
+                    'nro_documento' => trim($cli->nro_documento),
+                    'nom_cliente' => trim($cli->nom_cliente),
+                    'tellab' => trim($cli->tellab),
+                    'telefono' => trim($cli->telefono),
+                    'cel_alternativo' => trim($cli->cel_alternativo),
+                    'cel_lab' => trim($cli->cel_lab),
+                    'cel_part' => trim($cli->cel_part),
+                    'operacion' => trim($cli->operacion),
+                    'segmento' => '',
+                    'producto' => trim($cli->producto),
+                    'tipo_cred_tarj' => '',
+                    'tipo_ope' => '',
+                    'tasa' => '',
+                    'saldo_capital' => trim($cli->saldo_capital),
+                    'saldo_interes' => trim($cli->saldo_interes),
+                    'tipo_cambio' => '',
+                    'dias_mora' => trim($cli->dias_mora),
+                    'nro_cuota' => '1',
+                    'total_cuotas' => '1',
+                    'cuotas_pag' => '0',
+                    'cuotas_pend' => '1',
+                    'monto_mora' => '',
+                    'monto_cuota' => trim($cli->monto_cuota),
+                    'fecha_vto_cuota' => $vto->format('d/m/Y'),
+                    'ult_fech_pago' => '',
+                    'total_deuda' => '',
+                    'estado' => '',
+                    'fec_apertura' => $apertura->format('d/m/Y'),
+                    'cat_riesgo' => '',
+                    'dato_extra1' => trim($cli->dato_extra1),
+                    'dato_extra2' => trim($cli->dato_extra2),
+                    'dato_extra3' => trim($cli->dato_extra3),
+                    'dato_extra4' => trim($cli->dato_extra4),
+                    'dato_extra5' => '',
+                    'dato_extra6' => '',
+                    'dato_extra7' => '',
+                    'COD_GESTOR' => ''
                 ];
-                $lista[]= $fila;
-
+                $lista[] = $fila;
             }
         }
 
-        log::info('FIN GENERACION DE BASE CLIENTES RURAL COBRANZAS');
-        return collect($lista);
-    }
+        Log::info('FIN GENERACION DE BASE CLIENTES NEOTEL');
 
-    private function getClientes($fecha){
-        $clientes = DatosEjemplo::all();
-        return $clientes;
-    }
+        // Convertir la lista de datos a JSON
+        $json_data = json_encode($lista);
 
-    public function headings(): array
-    {
-        return [
-            'cod_cliente',
-            'nro_documento',
-            'nom_cliente',
-            'tellab',
-            'telefono',
-            'cel_alternativo',
-            'cel_lab',
-            'cel_part',
-            'operacion',
-            'segmento',
-            'producto',
-            'tipo_cred_tarj',
-            'tipo_ope',
-            'tasa',
-            'saldo_capital',
-            'saldo_interes',
-            'tipo_cambio',
-            'dias_mora',
-            'nro_cuota',
-            'total_cuotas',
-            'cuotas_pag',
-            'cuotas_pend',
-            'monto_mora',
-            'monto_cuota',
-            'fecha_vto_cuota',
-            'ult_fech_pago',
-            'total_deuda',
-            'estado',
-            'fec_apertura',
-            'cat_riesgo',
-            'dato_extra1',
-            'dato_extra2',
-            'dato_extra3',
-            'dato_extra4',
-            'dato_extra5',
-            'dato_extra6',
-            'dato_extra7',
-            'cod_gestor'
+        // URL del endpoint de la API
+        $url = 'http://10.19.150.80/neoapi/webservice.asmx';
+
+        // Parámetros adicionales para enviar a la API
+        $params = [
+            'idtask' => 39,
+            'param1' => 'Ejemplo1',
+            'param2' => $json_data
         ];
+
+        // Cabeceras HTTP para JSON
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+        ];
+
+        // Enviar la solicitud HTTP POST con los datos JSON y las cabeceras
+        $response = Http::withHeaders($headers)->post($url, $params);
+
+        // Verificar la respuesta de la API
+        if ($response->successful()) {
+            // Solicitud exitosa
+            return $response->json();
+        } else {
+            // Error en la solicitud
+            return response()->json(['error' => 'Error al enviar los datos a la API'], $response->status());
+        }
+    }
+
+    private function getClientes($fecha)
+    {
+        return DatosEjemplo::all();
     }
 }
